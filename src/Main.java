888ad0d00a1035f596cbe7687d36c24412e9fffd
Snehamol Selvaraj <snehamolselvaraj.com>
2025-07-31 00:23:24 +0530

    Initial commit with HelloWorld and Main program

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// TIP: To run the code, press the green Run button in IntelliJ or press Shift + F10
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Print welcome message
+        System.out.println("Hello and welcome!");
 
+        // Print numbers from 1 to 5
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
 }
-
